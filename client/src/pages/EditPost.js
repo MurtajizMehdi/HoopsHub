d03f199b4e4bd002194b9d78064b51ab71009b23
Murtajiz Mehdi <chicomehdi1234@gmail.com>
2024-04-27 02:01:04 -0400

    Added post editing functionality

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -2,13 +2,25 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import './EditPost.css'
 import { useState } from 'react';
+import { supabase } from '../client'
 
 const EditPost = ({data}) => {
 
     const {id} = useParams();
     const [post, setPost] = useState({id: null, title: "", author: "", description: ""});
 
-    console.log(id);
+    const updatePost = async (event) => {
+        event.preventDefault();
+
+        await supabase
+            .from('Posts')
+            .update({ title: post.title, author: post.author,  description: post.description})
+            .eq('id', id);
+
+        window.location = "/";
+    }
+
+    // console.log(id);
 
     const handleChange = (event) => {
         const {name, value} = event.target;
@@ -32,10 +44,9 @@ const EditPost = ({data}) => {
                 <br/>
 
                 <label for="description">Description</label><br />
-                <textarea rows="5" cols="50" id="description" value={post.description} onChange={handleChange} >
-                </textarea>
+                <input type="text" id="description" name="description" value={post.description} onChange={handleChange} /><br />
                 <br/>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit" onClick={updatePost}/>
                 <button className="deleteButton">Delete</button>
             </form>
         </div>
